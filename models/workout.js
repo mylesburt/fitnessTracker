dd1073b8c6b8397125c306e0b7685ea0c02a01a7
Myles Burt <myles_burt@rocketmail.com>
2021-01-17 22:09:31 +1030

    workouts updated with simple schema structre, needs to be filled.

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for transaction"
+      },
+      exercises: [
+          {
+              type: {
+                  type: String,
+              },
+              weight: {
+                  type: Number,
+              },
+              sets: {
+
+              }
+          }
+      ],
+
+
+
+
+
+
+      date: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
