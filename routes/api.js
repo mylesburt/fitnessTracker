f1f4a1145442121f9edec90540111720054cec9a
Myles Burt <myles_burt@rocketmail.com>
2021-01-17 23:01:20 +1030

    api route connection created.

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,43 @@
 const router = require("express").Router();
+const Workout = require("../models/workout");
+const { get } = require("./web");
 
 
 router.get("/api/workouts/range", (req, res) => {
-
-})
+  Workout.find({}).then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
 
 router.post("/api/workouts", (req, res) => {
-
-})
+  Workout.create({}).then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
 
 router.get("/api/workouts", (req, res) => {
-        Workout.find({})
-          .sort({ date: -1 })
-          .then(dbTransaction => {
-            res.json(dbTransaction);
-          })
-          .catch(err => {
-            res.status(400).json(err);
-          });
+  Workout.find({}).then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-
-})
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id }, { exercises: req.body }
+  ).then((dbWorkout) => {
+    res.json(dbWorkout);
+  }).catch(err => {
+    res.status(400).json(err);
+  });
+});
 
 module.exports = router;
