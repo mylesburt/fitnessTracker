12e4cad4fc626b4e02c97d9fe2da989ede2f55b0
Myles Burt <myles_burt@rocketmail.com>
2021-01-17 23:11:28 +1030

    Reshuffled date code to see if it made a difference.

--- models/workout.js
+++ models/workout.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+          date: {
+        type: Date,
+        default: Date.now
+      },
       exercises: [
           {
               name: {
@@ -29,10 +33,6 @@ const workoutSchema = new Schema({
               }
           }
       ],
-      date: {
-        type: Date,
-        default: Date.now
-      }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
