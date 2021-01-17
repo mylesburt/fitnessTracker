0e566ef96f62c1ec4b3e4c268ef23de266c61a0c
Myles Burt <myles_burt@rocketmail.com>
2021-01-17 22:07:51 +1030

    Establishing connections to routes and models folders.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const webRouter = require('./routes/web');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -7,8 +10,15 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/api")(app);
-require("./routes/web")(app);
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(webRouter);
+app.use(apiRouter);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
