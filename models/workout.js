18adecf83fe2bb3343111f48951b7316b7a55554
Myles Burt <myles_burt@rocketmail.com>
2021-01-17 22:42:39 +1030

    Schema completed.

--- models/workout.js
+++ models/workout.js
@@ -3,30 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for transaction"
-      },
       exercises: [
           {
+              name: {
+                  type: String,
+                  trim: true,
+                  required: true
+              },
               type: {
                   type: String,
+                  trim: true,
+                  required: true
               },
               weight: {
-                  type: Number,
+                type: Number,
               },
               sets: {
-
+                type: Number,
+              },
+              reps: {
+                type: Number,
+              },
+              duration: {
+                type: Number,
               }
           }
       ],
-
-
-
-
-
-
       date: {
         type: Date,
         default: Date.now
